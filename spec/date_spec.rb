186226df027fab4ba66e7cf16ad628ef6217ca29
oddityoverseer13 <derek.kniffin@gmail.com>
2015-09-01 13:13:46 -0400

    Refactored specs to new expect form

--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -1,45 +1,45 @@
 describe Date do
-  before(:each) do
-    @date = GEDCOM::Date.new("1 APRIL 2008")
-    @date_range_from = GEDCOM::Date.new("FROM APRIL 2007 TO JUNE 2008")
-    @date_range_between = GEDCOM::Date.new("BETWEEN 1 JANUARY 1970 AND 1 APRIL 2008")
-    @date_bc = GEDCOM::Date.new("25 JANUARY 1 BC")
-    @date_year_span = GEDCOM::Date.new("1 APRIL 2007/08")
-  end
+
+  let(:date) { GEDCOM::Date.new("1 APRIL 2008") }
+  let(:date_range_from) { GEDCOM::Date.new("FROM APRIL 2007 TO JUNE 2008") }
+  let(:date_range_between) { GEDCOM::Date.new("BETWEEN 1 JANUARY 1970 AND 1 APRIL 2008") }
+  let(:date_bc) { GEDCOM::Date.new("25 JANUARY 1 BC") }
+  let(:date_year_span) { GEDCOM::Date.new("1 APRIL 2007/08") }
+
 
   ## ! Could definitely stand to beef this test up. About, Estimated, etc.
   ##   Lot's of flags to test.
   it "makes flags available" do
-    (@date_range_from.format & GEDCOM::Date::FROMTO).should_not == 0
-    (@date_range_between.format & GEDCOM::Date::BETWEEN).should_not == 0
+    expect(date_range_from.format & GEDCOM::Date::FROMTO).to_not eq(0)
+    expect(date_range_between.format & GEDCOM::Date::BETWEEN).to_not eq(0)
   end
 
   it "does comparison" do
-    (@date <=> @date_bc).should == 1
-    (@date_bc <=> @date).should == -1
-    (@date <=> @date).should == 0
+    expect(date <=> date_bc).to eq(1)
+    expect(date_bc <=> date).to eq(-1)
+    expect(date <=> date).to eq(0)
   end
 
   it "gets first and last date from ranges" do
-    @date_range_from.is_range?.should == true
-    @date_range_between.is_range?.should == true
+    expect(date_range_from.is_range?).to be(true)
+    expect(date_range_between.is_range?).to eq(true)
 
-    @date_range_from.first.nil?.should == false
-    @date_range_from.last.nil?.should == false
-    @date_range_between.first.nil?.should == false
-    @date_range_between.last.nil?.should == false
+    expect(date_range_from.first.nil?).to eq(false)
+    expect(date_range_from.last.nil?).to eq(false)
+    expect(date_range_between.first.nil?).to eq(false)
+    expect(date_range_between.last.nil?).to eq(false)
 
-    (@date_range_from.first <=> @date_range_from.last).should == -1
-    (@date_range_between.first <=> @date_range_between.last).should == -1
+    expect(date_range_from.first <=> date_range_from.last).to eq(-1)
+    expect(date_range_between.first <=> date_range_between.last).to eq(-1)
   end
 
   # to_s currently works differently in the Ruby vs. C extension
   # code, therefore this test is failing (in C)
   it "converts to string" do
-    @date.to_s.should == "1 Apr 2008"
-    @date_range_from.to_s.should == "from Apr 2007 to Jun 2008"
-    @date_range_between.to_s.should == "bet 1 Jan 1970 and 1 Apr 2008"
-    @date_bc.to_s.should == "25 Jan 1 BC"
-    @date_year_span.to_s.should == "1 Apr 2007-8"
+    expect(date.to_s).to eq("1 Apr 2008")
+    expect(date_range_from.to_s).to eq("from Apr 2007 to Jun 2008")
+    expect(date_range_between.to_s).to eq("bet 1 Jan 1970 and 1 Apr 2008")
+    expect(date_bc.to_s).to eq("25 Jan 1 BC")
+    expect(date_year_span.to_s).to eq("1 Apr 2007-8")
   end
 end
--- spec/datepart_spec.rb
+++ spec/datepart_spec.rb
@@ -1,89 +1,87 @@
 describe DatePart do
-  before(:each) do
-    @date = GEDCOM::Date.new("1 APRIL 2008")
-    @date_range_from = GEDCOM::Date.new("FROM APRIL 2007 TO JUNE 2008")
-    @date_range_between = GEDCOM::Date.new("BETWEEN 1 JANUARY 1970 AND 1 APRIL 2008")
-    @date_bc = GEDCOM::Date.new("25 JANUARY 1 BC")
-    @date_year_span = GEDCOM::Date.new("1 APRIL 2007/08")
-    @nonstandard = GEDCOM::Date.safe_new("FIRST DAY OF 2008")
-    @phrase = GEDCOM::Date.safe_new("(independance day)")
-  end
+  let(:date) { GEDCOM::Date.new("1 APRIL 2008") }
+  let(:date_range_from) { GEDCOM::Date.new("FROM APRIL 2007 TO JUNE 2008") }
+  let(:date_range_between) { GEDCOM::Date.new("BETWEEN 1 JANUARY 1970 AND 1 APRIL 2008") }
+  let(:date_bc) { GEDCOM::Date.new("25 JANUARY 1 BC") }
+  let(:date_year_span) { GEDCOM::Date.new("1 APRIL 2007/08") }
+  let(:nonstandard) { GEDCOM::Date.safe_new("FIRST DAY OF 2008") }
+  let(:phrase) { GEDCOM::Date.safe_new("(independance day)") }
 
   it "makes date type and flags available" do
-    (@date.first.compliance | GEDCOM::DatePart::NONE).should == 0
-    #(@nonstandard.first.compliance & GEDCOM::DatePart::NONSTANDARD).should_not == 0
-    (@phrase.first.compliance & GEDCOM::DatePart::PHRASE).should_not == 0
+    expect(date.first.compliance | GEDCOM::DatePart::NONE).to eq(0)
+    #expect(nonstandard.first.compliance & GEDCOM::DatePart::NONSTANDARD).to_not eq(0)
+    expect(phrase.first.compliance & GEDCOM::DatePart::PHRASE).to_not eq(0)
 
-    (@date_range_from.first.calendar | GEDCOM::DateType::DEFAULT).should == 0
-    (@date_range_from.last.calendar | GEDCOM::DateType::DEFAULT).should == 0
+    expect(date_range_from.first.calendar | GEDCOM::DateType::DEFAULT).to eq(0)
+    expect(date_range_from.last.calendar | GEDCOM::DateType::DEFAULT).to eq(0)
   end
 
   it "finds days" do
-    @date.first.has_day?.should == true
-    @date.first.day.should == 1
+    expect(date.first.has_day?).to eq(true)
+    expect(date.first.day).to eq(1)
 
-    @date_range_between.first.has_day?.should == true
-    @date_range_between.first.day.should == 1
+    expect(date_range_between.first.has_day?).to eq(true)
+    expect(date_range_between.first.day).to eq(1)
 
-    @date_range_between.last.has_day?.should == true
-    @date_range_between.last.day.should == 1
+    expect(date_range_between.last.has_day?).to eq(true)
+    expect(date_range_between.last.day).to eq(1)
 
-    @date_bc.first.has_day?.should == true
-    @date_bc.first.day.should == 25
+    expect(date_bc.first.has_day?).to eq(true)
+    expect(date_bc.first.day).to eq(25)
   end
 
   it "finds months" do
-    @date.first.has_month?.should == true
-    @date.first.month.should == 4
+    expect(date.first.has_month?).to eq(true)
+    expect(date.first.month).to eq(4)
 
-    @date_range_between.first.has_month?.should == true
-    @date_range_between.first.month.should == 1
+    expect(date_range_between.first.has_month?).to eq(true)
+    expect(date_range_between.first.month).to eq(1)
 
-    @date_range_between.last.has_month?.should == true
-    @date_range_between.last.month.should == 4
+    expect(date_range_between.last.has_month?).to eq(true)
+    expect(date_range_between.last.month).to eq(4)
 
-    @date_bc.first.has_month?.should == true
-    @date_bc.first.month.should == 1
+    expect(date_bc.first.has_month?).to eq(true)
+    expect(date_bc.first.month).to eq(1)
   end
 
   it "finds years" do
-    @date.first.has_year?.should == true
-    @date.first.year.should == 2008
+    expect(date.first.has_year?).to eq(true)
+    expect(date.first.year).to eq(2008)
 
-    @date_range_between.first.has_year?.should == true
-    @date_range_between.first.year.should == 1970
+    expect(date_range_between.first.has_year?).to eq(true)
+    expect(date_range_between.first.year).to eq(1970)
 
-    @date_range_between.last.has_year?.should == true
-    @date_range_between.last.year.should == 2008
+    expect(date_range_between.last.has_year?).to eq(true)
+    expect(date_range_between.last.year).to eq(2008)
 
-    @date_bc.first.has_year?.should == true
-    @date_bc.first.year.should == 1
+    expect(date_bc.first.has_year?).to eq(true)
+    expect(date_bc.first.year).to eq(1)
   end
 
   it "finds the epoch" do
-    @date.first.epoch.should == "AD"
-    @date_bc.first.epoch.should == "BC"
+    expect(date.first.epoch).to eq("AD")
+    expect(date_bc.first.epoch).to eq("BC")
   end
 
   it "finds year span" do
-    @date_year_span.first.has_year_span?.should == true
-    @date.first.has_year_span?.should == false
+    expect(date_year_span.first.has_year_span?).to eq(true)
+    expect(date.first.has_year_span?).to eq(false)
   end
 
   # to_s currently works differently in the Ruby vs. C extension
   # code, therefore this test is failing (in C)
   it "converts to string" do
-    @date.first.to_s.should == "1 Apr 2008"
+    expect(date.first.to_s).to eq("1 Apr 2008")
 
-    @date_range_from.first.to_s.should == "Apr 2007"
-    @date_range_from.last.to_s.should == "Jun 2008"
+    expect(date_range_from.first.to_s).to eq("Apr 2007")
+    expect(date_range_from.last.to_s).to eq("Jun 2008")
 
-    @date_range_between.first.to_s.should == "1 Jan 1970"
-    @date_range_between.last.to_s.should == "1 Apr 2008"
+    expect(date_range_between.first.to_s).to eq("1 Jan 1970")
+    expect(date_range_between.last.to_s).to eq("1 Apr 2008")
 
-    @date_bc.first.to_s.should == "25 Jan 1 BC"
+    expect(date_bc.first.to_s).to eq("25 Jan 1 BC")
 
-    @date_year_span.to_s.should == "1 Apr 2007-8"
+    expect(date_year_span.to_s).to eq("1 Apr 2007-8")
   end
 
 end
